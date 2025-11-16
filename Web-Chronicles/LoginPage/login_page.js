ceb2f620f24fadf19964c08dd188119c03b577ce
Vondracmon <merlybernardloyola09@gmail.com>
2025-11-16 20:53:51 +0800

    firebase

--- Web-Chronicles/LoginPage/login_page.js
+++ Web-Chronicles/LoginPage/login_page.js
@@ -1,11 +1,21 @@
 class LoginPage {
     constructor() {
-        document.getElementById("loginBtn").addEventListener("click", function() {
-        window.location.href = "../mainpage/index.html";
+        document.getElementById("loginBtn").addEventListener("click", () => {
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+
+            firebase.auth().signInWithEmailAndPassword(email, password)
+                .then(() => {
+                    alert("Login successful!");
+                    window.location.href = "../mainpage/index.html";
+                })
+                .catch(error => {
+                    alert(error.message);
+                });
         });
     }
 }
 
-window.onload = function() {
+window.onload = () => {
     new LoginPage();
 };
---
+++ Web-Chronicles/firebase/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBF0nMdLXtvjWBsQAtHH60LxAFLeddI9I",
+  authDomain: "web-chronicles.firebaseapp.com",
+  databaseURL: "https://web-chronicles-default-rtdb.firebaseio.com",
+  projectId: "web-chronicles",
+  storageBucket: "web-chronicles.appspot.com",
+  messagingSenderId: "250178417089",
+  appId: "1:250178417089:web:29ff41b1864019d4efff40",
+  measurementId: "G-SVQGM57HNM"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
